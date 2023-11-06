8f11ccee0a29ec15429b95b730cd546696b87565
segunUb18 <eeomo@yahoo.com>
2023-11-06 16:36:51 +0100

    test case1

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
     '''Base configuration class
     '''
     # database URI for SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = 'postgresql://volunteers_ck6d_user:qxLwPQk0FFPc3GEhKggGTICqFpd4ec8j@dpg-cl4fivp828mc73fjbjg0-a/volunteers_ck6d'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://vo_user:y6ewqhkmCX72V5Axs09wSbHTBUqV7yJ8@dpg-cl4gek9828mc73cssa60-a/vo'
 
     # Disable track modifications to avoid warning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -16,7 +16,7 @@ class ProductionConfig(Config):
     '''Production configuration class
     '''
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://volunteers_ck6d_user:qxLwPQk0FFPc3GEhKggGTICqFpd4ec8j@dpg-cl4fivp828mc73fjbjg0-a/volunteers_ck6d'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://vo_user:y6ewqhkmCX72V5Axs09wSbHTBUqV7yJ8@dpg-cl4gek9828mc73cssa60-a/vo'
 
     # Mapping config names to their respective classes
 config_map = {
